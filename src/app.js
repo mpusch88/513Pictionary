fa12117565b35011c8b09d97a1f9a4868be8a27c
Matt Pusch <mpusch88@gmail.com>
2019-03-12 16:15:15 -0600

    update

--- src/app.js
+++ src/app.js
@@ -1,13 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './componenets/App';
+import React, {Component} from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-require('./index.html');
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <p>
+                        Edit
+                        <code>src/App.js</code>
+                        and save to reload.
+                    </p>
+                    <a
+                        className="App-link"
+                        href="https://reactjs.org"
+                        target="_blank"
+                        rel="noopener noreferrer">
+                        Learn React
+                    </a>
+                </header>
+            </div>
+        );
+    }
+}
 
-// Create app
-const container = document.querySelector('#app-container');
-
-// Render app
-ReactDOM.render(
-    <App/>
-);
+export default App;
--- src/componenets/App.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const App = () => <div className='container'>
-    <div className='row'>
-        Hello, World!
-    </div>
-</div>;
-
-export default App;
