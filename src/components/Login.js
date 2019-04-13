88a4de5bce6ab34df8263d7bef52f1572626ab4b
Matt Pusch <mpusch88@gmail.com>
2019-04-12 22:05:48 -0600

    semi-working sidebars...

--- src/components/Login.js
+++ src/components/Login.js
@@ -7,7 +7,6 @@ import {withRouter} from 'react-router-dom';
 import {send_loginfo} from '../api';
 import logo from '../resources/logo.png';
 import $ from 'jquery';
-// eslint-disable-next-line no-unused-vars
 import {cookie} from 'jquery.cookie';
 
 class Login extends React.Component {
